288650d45ec6204abdc5f4e4c6337ce58d71c0b9
HongQin <hqinlab@gmail.com>
2016-01-26 10:24:13 -0500

    plot with sd, error in "M1-2,20111207.modified.csv"

--- analysis/_1a.batchPlotH2O2-LOH.2016Jan25.R
+++ analysis/_1a.batchPlotH2O2-LOH.2016Jan25.R
@@ -1,17 +1,18 @@
-#batch plot, 2012 April 18
+# 20160125, try to add error bars to plots
 
 rm=(list=ls())
 setwd("~/github/LOH_H2O2_2016/analysis")
-debug = 5;
+debug = 0;
 
 #all data are in 'data.H2O2-LOH'
 FileList = list.files( path="../data.H2O2-LOH/");  FileList; 
 if( debug > 5) {FileList = FileList[1:2]}
 
+# infile = FileList[2]
 for( infile in FileList) {
 # infile='M1-2,20111207.modified.csv' #debug for NA, and low lead concentration
 # infile =  'M2-8,08172011.H2O2.LOH.modified.csv';  #viability plot error
-#infile =  'M2-8,06062011.H2O2onLOH.csv';  #b plot error
+# infile =  'M2-8,06062011.H2O2onLOH.csv';  #b plot error
    
 fullFileName = paste('../data.H2O2-LOH/',infile, sep='');
 mylabel = infile
@@ -27,7 +28,7 @@ for( i in 1:length(tb[,1])) {
   }
 }
 
-
+#H2O2 working stock is 2X 
 tb$H2O2 = tb$H2O2stock/2
 tb$tot = tb$White + tb$Black + tb$halfBlack + tb$quarterBlack + tb$ThreeQBlack + tb$QQBlack + tb$Other
 tb.ori = tb; 
@@ -49,9 +50,13 @@ for ( i in 1:length(H2O2)) {
   c = H2O2[i]
   tmp = tb[ tb$H2O2==c, ]	
   tbm$tot[i] = mean(tmp$tot, na.rm=T)
+  tbm$tot.sd[i] = sd(tmp$tot, na.rm=T)
   tbm$White[i] = mean(tmp$White, na.rm=T)
+  tbm$White.sd[i] = sd(tmp$White, na.rm=T)
   tbm$Black[i] = mean(tmp$Black, na.rm=T) 
+  tbm$Black.sd[i] = sd(tmp$Black, na.rm=T) 
   tbm$halfBlack[i] = mean(tmp$halfBlack, na.rm=T)  
+  tbm$halfBlack.sd[i] = sd(tmp$halfBlack, na.rm=T)  
   tbm$quarterBlack[i] = mean(tmp$quarterBlack, na.rm=T)
   tbm$ThreeQBlack[i] = mean(tmp$ThreeQBlack, na.rm=T)  
   tbm$QQBlack[i] = mean(tmp$QQBlack, na.rm=T);  
@@ -65,22 +70,29 @@ tbm = tbm[tbm$tot>1, ] #remove plates with zero colonies
 ###### calculate fractions
 tbf = tbm; 
 tbf$s = tbf$tot / max(tbf$tot)
-for ( j in 3:8) {
+tbf$s.sd = tbm$tot.sd / max(tbm$tot)
+for ( j in 3:11) {
   tbf[, j] = tbf[,j] / tbf$tot
 }
 
 tbf$Black[tbf$Black<0]=NA;  #remove weird experimental data, such as low-lead concentration effect
 
-pdf(paste("sandbox/",infile, ".batch.pdf", sep=''), width=5, height=5)
+pdf(paste("sandbox/",infile, ".batch.sd.pdf", sep=''), width=5, height=5)
 
 ### full black plot
-plot( tbf$s ~ tbf$H2O2, col="blue", axes=F, xlab='', ylab='', ylim=c(0, 1.2), type='p');
+plot( tbf$s ~ tbf$H2O2, col="blue", axes=F, xlab='', ylab='', ylim=c(-0.05, 1.2), type='p');
 lines( tbf$s ~ tbf$H2O2, col="blue",lty=2)
-axis( 4, at=pretty(c(0, 1.2)))
-legend ( max(H2O2)*0.7, 0.5, c("viability","black"), col=c("blue","black"), lty=c(2,1), pch=c(1,16) )
+arrows( tbf$H2O2, (tbf$s - tbf$s.sd), tbf$H2O2, (tbf$s + tbf$s.sd), length=0.1, angle=90,code=3, lty=1, col="blue" );
+
+axis( 4, at=pretty(c(0, 1.2)), col='black')
+legend ( max(H2O2)*0.7, 0.9, c("viability","black"), col=c("blue","black"), lty=c(2,1), pch=c(1,16) )
 par(new=T)
-plot( tbf$Black ~ tbf$H2O2, pch=16, xlab='H2O2',ylab="black")
+plot( tbf$Black ~ tbf$H2O2, pch=16, xlab='H2O2',ylab="black", ylim=c(-0.01, max(tbf$Black)*3))
 lines(tbf$Black ~ tbf$H2O2)
+bottoms = tbf$Black -tbf$Black.sd
+bottoms[bottoms<0]= 0.001 
+arrows( tbf$H2O2, bottoms, tbf$H2O2, (tbf$Black + tbf$Black.sd), length=0.1, angle=90,code=3, lty=1, col="gray" );
+
 title(mylabel)
 
 ## full black -log plot
